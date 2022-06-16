66d1b79fb9f038b1d1e62f1241342172dc6ae198
unknown <abhisheksrivastava9719@gmail.com>
2022-06-16 15:25:35 +0530

    Leap year program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Welcome to Day 5 assignment");
+﻿Console.WriteLine("Enter a year you want to check for Leap Year in the format yyyy");
+int y=Convert.ToInt32(Console.ReadLine());
+int ye = y;                  //alternate variable for year
+int d = 0;                         //digit variable
+while(y>0)                  //to check no. of digits of the year
+{
+    d++;
+    y = y / 10;
+}
+if(d>3&&d<5)
+{
+    if(ye%4==0)
+    {
+        if(ye%400==0)                       //Checking if the year is a century and a leap year
+        {
+            Console.WriteLine("This century is a leap year");
+        }
+        else
+        {
+            Console.WriteLine("This year is a leap year");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Not a leap year");
+    }
+}
+else
+{
+    Console.WriteLine("Wrong input");
+}
