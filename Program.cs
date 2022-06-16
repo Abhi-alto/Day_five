64deaf64bcdeaba24d190b842be11d0d80bd65ed
unknown <abhisheksrivastava9719@gmail.com>
2022-06-16 17:45:40 +0530

    Remainder and quotient found

--- Program.cs
+++ Program.cs
@@ -1,20 +1,6 @@
-﻿using Day_five_assignment;
-Console.WriteLine("Enter a value whose prime factors you want");
+﻿Console.WriteLine("Enter numerator");
 int n=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Prime factorization of " + n + " = ");
-for (int i = 2; i <= (n / 2); i++)            //factors are only from 2 to the half of the number
-{
-    while (n % i == 0)              
-    {
-        Console.WriteLine(i + " ");
-        n = n / i;                          //remove that particular factor by updating the number value
-    }
-}
-if(n>1)
-{
-    Console.WriteLine(n);
-}
-else
-{
-    Console.WriteLine("Error");
-}
+Console.WriteLine("Enter any denominator");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Quotient is " +(n/m));
+Console.WriteLine("Remainder is " + (n % m));
